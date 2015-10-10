fdbe85921ae17af66f289d86763b8281e7ead664
amiruqdah <ottoman.empire12@pacbell.net>
2015-10-10 13:52:16 -0700

    basic squash collider functionality

--- Assets/SquashCollider.cs
+++ Assets/SquashCollider.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class SquashCollider : MonoBehaviour
 {
-
-    // Get collider 
-
-    // Get thing that coolides and basically go and type that happens have for hting
     void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        Debug.Log(hit.gameObject.name);
+        var normal = hit.normal;
+
+         Debug.Log("test");
+        
     }
 }
