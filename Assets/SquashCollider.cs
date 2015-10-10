d8a8e0bafd23f345e1e56c30561472becb63c2a9
amiruqdah <ottoman.empire12@pacbell.net>
2015-10-10 13:02:49 -0700

    new enviornment models added

---
+++ Assets/SquashCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquashCollider : MonoBehaviour
+{
+
+    // Get collider 
+
+    // Get thing that coolides and basically go and type that happens have for hting
+    void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        Debug.Log(hit.gameObject.name);
+    }
+}
