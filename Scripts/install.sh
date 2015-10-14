37165911a313c05f5650f9cf857d6633f5f914e1
Amir Uqdah <ottoman.empire12@pacbell.net>
2015-10-14 00:02:15 -0700

    made a slight modification to install script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-echo 'Downloading from http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg: '
-curl -o Unity.pkg http://netstorage.unity3d.com/unity/afd2369b692a/MacEditorInstaller/Unity-5.1.2f1.pkg
+echo 'Downloading from http://netstorage.unity3d.com/unity/44735ea161b3/MacEditorInstaller/Unity-5.2.1f1.pkg'
+curl -o Unity.pkg http://netstorage.unity3d.com/unity/44735ea161b3/MacEditorInstaller/Unity-5.2.1f1.pkg
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
