7f8b2c45590247d34d8f2d6d6a59c07ffdb3c124
Amir Uqdah <amiruqdah@users.noreply.github.com>
2015-10-20 13:07:29 -0700

    cleaned up spawn script
    fixed Lucas's :shit:

--- Assets/Scripts/Player/CharacterDecider.cs
+++ Assets/Scripts/Player/CharacterDecider.cs
@@ -3,52 +3,32 @@ using System.Collections;
 using HappyFunTimes;
 
 public class CharacterDecider : MonoBehaviour {
-
-	public GameObject mario;
-	public GameObject luigi;
-	public GameObject purpleMario;
-	public GameObject yellowLuigi;
-	public GameObject flyGuy;
-
-	// Use this for initialization
-	void Start () 
-	{
-
-	}
-
+	
+	// SET THESE IN THE INSPECTOR
+	public GameObject mario; 		// a reference to the Mario Prefab GameObject
+	public GameObject luigi;		// a reference to the Luigi Prefab GameObject
+	public GameObject purpleMario;		// a reference to the PurpleMario Prefab GameObject
+	public GameObject yellowLuigi;		// a reference to the YellowLuigi Prefab GameObject
+	public GameObject flyGuy;		// a reference to the FlyGuy Prefab GameObejct
+	
 	void InitializeNetPlayer(SpawnInfo spawnInfo) 
 	{
 		NetPlayer m_netPlayer = spawnInfo.netPlayer;
 
 		GameObject newChar;
 		if (!GameObject.FindWithTag ("Mario")) 
-		{
 			newChar = Instantiate(mario);
-		} 
 		else if (!GameObject.FindWithTag ("Luigi")) 
-		{
 			newChar = Instantiate(luigi);
-		} 
 		else if (!GameObject.FindWithTag ("PurpleMario")) 
-		{
 			newChar = Instantiate(purpleMario);
-		} 
 		else if (!GameObject.FindWithTag ("YellowLuigi")) 
-		{
 			newChar = Instantiate(yellowLuigi);
-		} 
 		else 
-		{
 			newChar = Instantiate(flyGuy);
-		}
 
 		newChar.GetComponent<HFTGamepad>().InitializeNetPlayer (m_netPlayer);
-
 		Destroy (this.gameObject);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        // TODO: Leaderboard Update Here
 }
